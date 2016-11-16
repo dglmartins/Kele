4fd3a87b40b320bb97c71fa7898e766b86b15102
danilomartins <dglmartins@outlook.com>
2016-11-16 11:53:08 +0800

    checkpoint 3 retrieve users completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,18 +4,23 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    @options = {
+    options = {
         body: {
           email: email,
           password: password
         }
     }
+    auth_response = self.class.post("/sessions", options)
 
+    @auth_response_body = JSON.parse(auth_response.body)
+
+    @auth_token = @auth_response_body["auth_token"]
   end
 
-  def get_auth_token
-    @response = self.class.post("/sessions", @options)
-    puts @response
+  def get_me
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token})
+    @me_response_body = JSON.parse(response.body)
+    
   end
 
 end
